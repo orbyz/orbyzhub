8299ac09c72eb8e23b9531a47fa0ea67e9e009bc
Jonathan Olbes <olbes.es@gmail.com>
2026-07-08 07:47:12 +0200

    feat(sidebar): create sidebar foundation

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,3 +1,10 @@
 export function Sidebar() {
-  return <aside className="h-screen w-70 shrink-0 border-r p-4">Sidebar</aside>;
+  return (
+    <aside
+      className="h-screen shrink-0 border-r p-4"
+      style={{ width: "var(--layout-sidebar-width)" }}
+    >
+      Sidebar
+    </aside>
+  );
 }
